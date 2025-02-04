a3a748f1d07e8355c4a24706f7a8c6b1465efdc4
H3xaChad <LordLayer@gmx.de>
2025-02-04 13:13:18 +0100

    Updated gitignore

--- src/backend/OnlyBans.Backend/Program.cs
+++ src/backend/OnlyBans.Backend/Program.cs
@@ -6,6 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("PostgresConnection")));
 
+builder.Services.AddRouting(options => options.LowercaseUrls = true);
+
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
--- src/backend/OnlyBans.Backend/obj/Debug/net8.0/OnlyBans.Backend.AssemblyInfo.cs
+++ src/backend/OnlyBans.Backend/obj/Debug/net8.0/OnlyBans.Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OnlyBans.Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+856a232915033c24156f01512053d2863034994f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+44a80d039689b16aef6ed5aaf0d6f7ce551ece2a")]
 [assembly: System.Reflection.AssemblyProductAttribute("OnlyBans.Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OnlyBans.Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
