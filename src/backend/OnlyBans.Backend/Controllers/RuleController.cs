4848befbf3b3f2ef90be4a5ef5b3a0b75037252e
Robin Kaiser <158357923+xrRyZe@users.noreply.github.com>
2025-02-06 13:38:20 +0100

    Add: rule api handling

---
+++ src/backend/OnlyBans.Backend/Controllers/RuleController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.EntityFrameworkCore;
+using OnlyBans.Backend.Data;
+using OnlyBans.Backend.Models;
+using OnlyBans.Backend.Models.Posts;
+using OnlyBans.Backend.Models.Rules;
+using OnlyBans.Backend.Models.Users;
+
+namespace OnlyBans.Backend.Controllers;
+
+[Route("api/v1/[controller]")]
+[ApiController]
+public class RuleController(AppDbContext context) : ControllerBase {
+    
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Rule>>> GetRules() {
+        var rules = await context.Rules.ToListAsync();
+        return Ok(rules);
+    }
+
+    [HttpGet("{id:guid}")]
+    public async Task<ActionResult<Rule>> GetRule(Guid id)
+    {
+        var rule = await context.Rules.FindAsync(id);
+        if (rule == null)
+            return NotFound();
+
+        return Ok(rule);
+    }
+}
