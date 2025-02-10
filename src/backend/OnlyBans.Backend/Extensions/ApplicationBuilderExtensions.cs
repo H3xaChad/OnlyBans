dc868553ba589a5cb575f5d8c0c4cd59e564737e
H3xaChad <LordLayer@gmx.de>
2025-02-10 13:15:57 +0100

    Added OpenAPI specifications

--- src/backend/OnlyBans.Backend/Extensions/ApplicationBuilderExtensions.cs
+++ src/backend/OnlyBans.Backend/Extensions/ApplicationBuilderExtensions.cs
@@ -3,8 +3,10 @@ namespace OnlyBans.Backend.Extensions;
 public static class ApplicationBuilderExtensions {
     public static void ConfigureMiddleware(this WebApplication app) {
         if (app.Environment.IsDevelopment()) {
-            app.UseSwagger();
-            app.UseSwaggerUI();
+            app.UseSwagger(c => c.RouteTemplate = "api/swagger/{documentName}/swagger.json");
+            app.UseSwaggerUI(c => { c.SwaggerEndpoint("/api/swagger/v1/swagger.json", "OnlyBans"); });
+            app.UseDeveloperExceptionPage();
+            app.UseCors("DevCors");
         }
 
         app.UseHttpsRedirection();
--- src/backend/OnlyBans.Backend/Program.cs
+++ src/backend/OnlyBans.Backend/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 using OnlyBans.Backend.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -18,7 +19,19 @@ builder.Services.AddOAuth2Authentication(builder.Configuration);
 builder.Services.AddRouting(options => options.LowercaseUrls = true);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(opt => {
+    opt.SwaggerDoc("v1",
+        new OpenApiInfo {
+            Title = "OnlyBans",
+            Version = "v1",
+            Description = @"API Description for OnlyBans"
+        });
+    opt.ResolveConflictingActions(apiDescriptions => apiDescriptions.First());
+    opt.MapType<DateOnly>(() => new OpenApiSchema {
+        Type = "string",
+        Format = "date"
+    });
+});
 
 var app = builder.Build();
 app.ConfigureMiddleware();
