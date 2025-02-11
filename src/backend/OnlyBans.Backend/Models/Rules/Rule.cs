1cdfdc3b740cef24f59bf79c4d9f2f12c364d1ac
Robin Kaiser <158357923+xrRyZe@users.noreply.github.com>
2025-02-11 10:42:50 +0100

    Rule Update

--- src/backend/OnlyBans.Backend/Models/Rules/Rule.cs
+++ src/backend/OnlyBans.Backend/Models/Rules/Rule.cs
@@ -1,4 +1,6 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using OnlyBans.Backend.Models.Users;
 
 namespace OnlyBans.Backend.Models.Rules;
 
@@ -10,4 +12,17 @@ public class Rule {
     public string Text { get; init; } = null!;
     
     public string RuleCategory { get; init; } = null!;
+    
+    [ForeignKey(nameof(User))]
+    public Guid UserId { get; init; }
+
+    public User User { get; init; } = null!;
+
+    public Rule ToRule(Guid userId) {
+        return new Rule {
+            Text = Text,
+            RuleCategory = RuleCategory,
+            UserId = userId
+        };
+    }
 }
